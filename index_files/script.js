231267e8d643b343082d541bb7bf1d95c94e3556
Misirlou <diogotbasto@gmail.com>
2014-06-21 11:22:09 +0100

    add website

---
+++ index_files/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+	$('#leftnav a').removeClass("active");
+
+        $("#container div").removeClass("active");
+	if($(window.location.hash).length<1){
+		 $("#Aboutme").addClass("active");
+	$('#Aboutmea').addClass("active");
+		 
+		
+		 }
+    else{
+	$(window.location.hash).addClass("active");
+	$(window.location.hash+'a').addClass("active");
+	}
+});
+window.onhashchange = function () {
+$('#leftnav a').removeClass("active");
+
+        $("#container div").removeClass("active");
+	if($(window.location.hash).length<1){
+		 $("#Aboutme").addClass("active");
+		$('#Aboutmea').addClass("active");
+		 }
+    else{
+	$(window.location.hash).addClass("active");
+	$(window.location.hash+'a').addClass("active");
+	}
+}
