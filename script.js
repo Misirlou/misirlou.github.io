e00116c7cd84853c7c2c4699000ba1206be72728
Misirlou <diogotbasto@gmail.com>
2014-10-15 11:40:23 +0100

    Update website

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
 $(document).ready(function() {
 	$('#leftnav a').removeClass("active");
-
-        $("#container div").removeClass("active");
+	$("#container div").removeClass("active");
 	if($(window.location.hash).length<1){
-		 $("#Aboutme").addClass("active");
-	$('#Aboutmea').addClass("active");
-		 
-		
-		 }
-    else{
-	$(window.location.hash).addClass("active");
-	$(window.location.hash+'a').addClass("active");
+		$("#Aboutme").addClass("active");
+		$('#Aboutmea').addClass("active");
+	}
+    else
+	{
+		$(window.location.hash).addClass("active");
+		$(window.location.hash+'a').addClass("active");
 	}
 });
 window.onhashchange = function () {
-$('#leftnav a').removeClass("active");
-
-        $("#container div").removeClass("active");
+	$('#leftnav a').removeClass("active");
+	$("#container div").removeClass("active");
 	if($(window.location.hash).length<1){
 		 $("#Aboutme").addClass("active");
 		$('#Aboutmea').addClass("active");
